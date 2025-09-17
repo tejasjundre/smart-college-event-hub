3cae2ffe41253ed5af808fa9a3df7d40eacd70bb
tejasjundre <jundretejas302inc@gmail.com>
2025-09-17 19:00:44 +0530

    Added missing dependencies and updated backend for deployment

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api", // change for deployment
+const API = axios.create({
+  baseURL: "https://smart-college-event-hub-2.onrender.com/api"
 });
 
-export default api;
+export default API;
